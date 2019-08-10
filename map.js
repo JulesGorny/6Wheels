42c87570b44f05b64576065e46e36c84b826ddc3
David CHARETTE <dc@lexico-consulting.com>
2019-08-10 20:52:30 +0200

    Markers and associated content on map from imgur posts

--- map.js
+++ map.js
@@ -1,18 +1,20 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    var mymap = L.map('mapid').setView([45.815010, 15.981919], 5);
+    var map = L.map('mapid').setView([45.815010, 15.981919], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 20,
         id: 'mapbox.streets',
         accessToken: 'token.to.enter'
-    }).addTo(mymap);
+    }).addTo(map);
 
-    var marker1 = L.marker([46.566250, 3.335290]).addTo(mymap);
-    $(marker1).bind('click', {id: 1}, onMarkerClick);
+    readPosts(map);
+    
+    var marker1 = L.marker([46.566250, 3.335290]).addTo(map);
+    $(marker1).bind('click', {id: 11}, onMarkerClick);
 
-    var marker2 = L.marker([48.862725, 2.287592]).addTo(mymap);
-    $(marker2).bind('click', {id: 2}, onMarkerClick);
+    var marker2 = L.marker([48.862725, 2.287592]).addTo(map);
+    $(marker2).bind('click', {id: 12}, onMarkerClick);
 });
 
 function onMarkerClick(event) {
@@ -21,4 +23,62 @@ function onMarkerClick(event) {
 
 function closeSlidePanel(i) {
     $(".js-cd-panel-" + i).removeClass("cd-panel--is-visible")
+}
+
+
+function createSlidePanel(index, title, photos_count, text) {
+    var html = "";
+
+    html += "<div class=\"cd-panel cd-panel--from-right js-cd-panel-" + index + "\" style=\"z-index: 999\">";
+    html += "<header class=\"cd-panel__header\">";
+    html += "<h1>" + title + "</h1>";
+    html += "<a onClick=\"closeSlidePanel(" + index + ");\" class=\"cd-panel__close js-cd-close\">Close</a>";
+    html += "</header>";
+    html += "<div class=\"cd-panel__container\">";
+    html += "<div class=\"cd-panel__content\">";
+    html += text;
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+
+    return $.parseHTML(html)[0];
+}
+
+function readPosts(map) {
+    $.ajax({
+		type: 'GET',
+        url: './posts/posts_lists.json',
+		dataType: 'json',
+		success: function(json) {
+			json.posts.forEach(function (post, index)  {
+                // For each post we have from Imgur
+                console.log(index + ' - ' + post);
+                $.ajax({
+                    type: 'GET',
+                    url: './posts/' + post + '/content.json',
+                    dataType: 'json',
+                    success: function(json) {
+                        console.log(json.photos_count);
+                        console.log(json.text_fr);
+                        console.log(json.text_en);
+                        console.log(json.lat);
+                        console.log(json.long);
+                        // With the post content, we create a new slide panel
+                        var postElement = createSlidePanel(index, post, json.photos_count, json.text_fr);
+                        $( "#posts_panels" ).append(postElement);
+
+                        // and we associate it with a marker on the map
+                        var marker = L.marker([json.lat, json.lat]).addTo(map);
+                        $(marker).bind('click', {id: index}, onMarkerClick);
+                    },
+                    error: function(err) {
+                        console.log('error', err);
+                    }
+                });
+            });
+		},
+		error: function(err) {
+			console.log('error', err);
+		}
+    });
 }
