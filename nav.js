d19f27ad34062260ee777d22816a0cac9cacef33
JulesGorny <jules.gorny@gmail.com>
2019-08-13 18:56:44 +0200

    navbar trads

--- nav.js
+++ nav.js
@@ -9,16 +9,16 @@ document.write('\
         <div class="collapse navbar-collapse" id="navbarSupportedContent">\
             <ul class="navbar-nav ml-auto">\
                 <li class="nav-item">\
-                    <a class="nav-link active" href="index.html">Home</a>\
+                    <a id="navbar_home" class="nav-link active" href="index.html"></a>\
                 </li>\
                 <li class="nav-item">\
-                    <a class="nav-link" href="map.html">Map</a>\
+                    <a id="navbar_map" class="nav-link" href="map.html"></a>\
                 </li>\
                 <li class="nav-item">\
                     <a class="nav-link" href="mossyearth.html">Mossy Earth</a>\
                 </li>\
                 <li class="nav-item last-link">\
-                    <a class="nav-link" href="aboutus.html">About Us</a>\
+                    <a id="navbar_APropos" class="nav-link" href="aboutus.html"></a>\
                 </li>\
                 <li class="nav-item flags">\
                     <input type="image" src="img/flag_fr.svg" onclick="translateTo(\'fr\');" />\
