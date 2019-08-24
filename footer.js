04fee12a8f2d2b8a1f771dce9ee30a267fd5d0d5
JulesGorny <jules.gorny@gmail.com>
2019-08-24 10:08:46 +0200

    using footer everywhere

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write('\
-<div style="margin-top: 2rem; padding-bottom: 0.25rem; z-index:999" class="footer bg-light textCenter">\
+<div style="margin-top: 2rem; padding-bottom: 0.25rem; z-index:999" class="footer textCenter">\
 	<span style="font-size: 0.75rem;">\
 		© 2019 Copyright : 6Wheels\
 		<a href="https://www.facebook.com/6WheelsAdventure" target="_blank" style="padding-left: 0.75rem; color: inherit; text-decoration: inherit;">\
