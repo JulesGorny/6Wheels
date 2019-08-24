c6ed3b73a9c880aa185df6ed8243a4936f16e81f
JulesGorny <jules.gorny@gmail.com>
2019-08-24 10:16:35 +0200

    repair navbar

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 document.write('\
-    <nav id="navbar" class="navbar fixed-top navbar-expand-md">\
+    <nav id="navbar" class="navbar fixed-top navbar-expand-md navbar-light">\
         <a class="navbar-brand" href="index.html">\
             <img src="img/6WheelsFullLogo.png"/>\
         </a>\
