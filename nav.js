8ed4682e99488de6332e41757d5730cd684e85b2
JulesGorny <jules.gorny@gmail.com>
2019-08-24 10:07:34 +0200

    using footer everywhere

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 document.write('\
-    <nav id="navbar" class="navbar navbar-expand-md bg-light navbar-light fixed-top" style="background-color:#0000004D !important;">\
+    <nav id="navbar" class="navbar navbar-expand-md bg-light navbar-light fixed-top">\
         <a class="navbar-brand" href="index.html">\
             <img src="img/6WheelsFullLogo.png"/>\
         </a>\
