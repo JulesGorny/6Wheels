25019cafeac8f69a7fb4ae7d12da25e31d10f514
JulesGorny <jules.gorny@gmail.com>
2019-08-24 10:11:36 +0200

    white icons on footer

--- footer.js
+++ footer.js
@@ -2,10 +2,10 @@ document.write('\
 <div style="margin-top: 2rem; padding-bottom: 0.25rem; z-index:999" class="footer textCenter">\
 	<span style="font-size: 0.75rem;">\
 		© 2019 Copyright : 6Wheels\
-		<a href="https://www.facebook.com/6WheelsAdventure" target="_blank" style="padding-left: 0.75rem; color: inherit; text-decoration: inherit;">\
+		<a href="https://www.facebook.com/6WheelsAdventure" target="_blank" style="padding-left: 0.75rem;">\
 			<i class="fab fa-facebook"></i>\
 		</a>\
-		<a href="https://www.instagram.com/6wheelsadventure" target="_blank" style="padding-left: 0.5rem; color: inherit; text-decoration: inherit;">\
+		<a href="https://www.instagram.com/6wheelsadventure" target="_blank" style="padding-left: 0.5rem;">\
 			<i class="fab fa-instagram"></i>\
 		</a>\
 	</span>\
