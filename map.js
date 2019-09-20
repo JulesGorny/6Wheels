5c93edc554533b83505ca93b8c92ba8181129a69
JulesGorny <jules.gorny@gmail.com>
2019-09-20 21:03:56 +0200

    link in posts now cliclable

--- map.js
+++ map.js
@@ -32,24 +32,9 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
-function removeDuplicates(num) {
-    var x,
-        len=num.length,
-        out=[],
-        obj={};
-   
-    for (x=0; x<len; x++) {
-      obj[num[x]]=0;
-    }
-    for (x in obj) {
-      out.push(x);
-    }
-    return out;
-  }
-
 function tranformRawTextToHtml (text) {
     // URL to clickable links
-    var regexUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+    var regexUrl = /(http:\/\/www.|https:\/\/www.|www.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm;
     var urls = text.match(regexUrl);
 
     if (urls) {
